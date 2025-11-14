b49acfa248525e0687f39cbc7061fc0435ff2e5f
artin <artin@cat.ms>
2025-11-15 01:10:01 +0800

    Remove unused file

--- sudoq-app/sudoqapp/src/main/kotlin/de/sudoq/controller/AppBarLayoutBehavior.kt
+++
@@ -1,8 +0,0 @@
-package de.sudoq.controller
-
-import com.google.android.material.appbar.AppBarLayout
-
-/**
- * Created by timo on 02.09.15.
- */
-class AppBarLayoutBehavior : AppBarLayout.Behavior() //todo no known usage...
